eec7955858bfb29ceb7633c8eb7329878f81c673
Mateie <matemachitidze2001@gmail.com>
2023-01-12 15:50:20 -0800

    Added membertable (WIP)

--- src/components/guild/MemberTable.tsx
+++ src/components/guild/MemberTable.tsx
@@ -1,6 +1,30 @@
+import { useQuery } from "@apollo/client";
+
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+
+import { FetchMembers } from "../../gql/queries/guilds";
+
+const columns: GridColDef[] = [
+    { field: "memberUsername", headerName: "Username", width: 150, sortable: false }
+];
+
 const MemberTable = ({ guild }: { guild: any }) => {
+    const { loading, data: { members } = {} } = useQuery(FetchMembers, {
+        variables: {
+            guildId: guild.id,
+            first: 5
+        }
+    });
+
+    if (loading) return <></>;
+
+    const rows = members.map((member: any, i: number) => ({
+        id: i,
+        memberUsername: member.user.tag
+    }));
+
     return (
-        <></>
+        <DataGrid columns={columns} rows={rows} rowsPerPageOptions={[5]}></DataGrid>
     );
 };
 
