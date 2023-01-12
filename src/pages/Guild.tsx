5b96bc5d30744b04a62699861051153da6463dd2
Mateie <matemachitidze2001@gmail.com>
2023-01-12 14:57:29 -0800

    Added invites for servers that bot not is in

--- src/pages/Guild.tsx
+++ src/pages/Guild.tsx
@@ -8,6 +8,8 @@ import { FetchGuild } from "../gql/queries/guilds";
 import MemberTable from "../components/guild/MemberTable";
 import GuildInfo from "../components/guild/GuildInfo";
 
+const inviteUrl = "https://discord.com/oauth2/authorize?client_id=969414951292788766&permissions=1634569944311&scope=bot";
+
 const Guild = ({ auth, bot }: { auth: any, bot: any }) => {
     const { guildId } = useParams();
     const { loading, data: { guild } = {} } = useQuery(FetchGuild, {
@@ -18,7 +20,10 @@ const Guild = ({ auth, bot }: { auth: any, bot: any }) => {
     });
 
     if (loading) return <></>;
-    if (!guild) return <Navigate to="/" replace={true} />;
+    if (!guild) {
+        window.open(inviteUrl, "_blank");
+        return <Navigate to="/" replace={true} />;
+    }
 
     return (
         <div className="guild-container">
