34c982605a2316acead84c3f1ee53d70db83337c
Mateie <matemachitidze2001@gmail.com>
2023-01-12 14:49:20 -0800

    Refactored code

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -7,15 +7,14 @@ import Typography from "@mui/material/Typography";
 import { ButtonGroup } from "@mui/material";
 import ProfileButton from "./user/ProfileButton";
 
+const authUrl = process.env.NODE_ENV === "production"
+    ? "https://discord.com/api/oauth2/authorize?client_id=969414951292788766&redirect_uri=https%3A%2F%2Fkuramisa.com%2Flogin&response_type=code&scope=identify%20guilds"
+    : "https://discord.com/api/oauth2/authorize?client_id=969414951292788766&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code&scope=identify%20guilds";
+
+const inviteUrl = "https://discord.com/oauth2/authorize?client_id=969414951292788766&permissions=1634569944311&scope=bot";
 
 const Navigation = ({ bot, auth }: { bot: any; auth: any; }) => {
     const navigate = useNavigate();
-
-    const authUrl = process.env.NODE_ENV === "production"
-        ? "https://discord.com/api/oauth2/authorize?client_id=969414951292788766&redirect_uri=https%3A%2F%2Fkuramisa.com%2Flogin&response_type=code&scope=identify%20guilds"
-        : "https://discord.com/api/oauth2/authorize?client_id=969414951292788766&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin&response_type=code&scope=identify%20guilds";
-
-    const inviteUrl = "https://discord.com/oauth2/authorize?client_id=969414951292788766&permissions=1634569944311&scope=bot";
 
     return (
         <AppBar position="static" sx={{
