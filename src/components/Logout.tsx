ff9601b3e10d35ba2ae8a5f749dc57ad0e4439b9
Mateie <matemachitidze2001@gmail.com>
2023-01-20 14:07:59 -0800

    Added a message for logging out

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -2,11 +2,16 @@ import { Navigate } from "react-router-dom";
 import { useContext } from "react";
 
 import { AuthContext } from "../providers/AuthProvider";
+import { MessageContext } from "../providers/MessageProvider";
 
 const Logout = () => {
     const { auth, logout } = useContext(AuthContext);
+    const { setMessage } = useContext(MessageContext);
 
-    if (auth) logout();
+    if (auth) {
+        logout();
+        setMessage({ message: "Logged out successfully", severity: "success" });
+    }
 
     return <Navigate to="/" replace={true} />;
 };
