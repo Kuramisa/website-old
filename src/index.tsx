f4a24dded191a999f20216673a15e8614ea8a551
Mateie <matemachitidze2001@gmail.com>
2023-01-19 18:31:23 -0800

    updated console logging

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,18 @@
 import { createRoot } from "react-dom/client";
 import ApolloProvider from "./providers/ApolloProvider";
 
+if (process.env.NODE_ENV !== "development") {
+    console.log("%cStop!", "color: red; font-size: 40px; font-weight: bold;");
+    console.log(
+        "%cDo not enter any scripts or some sort of \"hack\" here, it will most likely compromise your account and steal your information",
+        "font-size: 30px; font-weight: bold;"
+    );
+    console.log(
+        "%cThis browser feature is intended for developers only",
+        "font-size: 30px; font-weight: bold;"
+    );
+}
+
 const container = document.getElementById("app") as HTMLElement;
 const root = createRoot(container);
 
