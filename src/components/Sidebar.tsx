178e49bf430fef8c98cbfcefa91762691df2309c
Mateie <matemachitidze2001@gmail.com>
2023-01-19 19:51:06 -0800

    Disabled user servers for now (authentication issues on backend, fix in progress)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,22 +2,15 @@ import "../assets/less/sidebar.less";
 
 import { useRef, useState } from "react";
 
-import Button from "@mui/material/Button";
-
 import BotGuildScroll from "./scroll/BotGuildScroll";
 import UserGuildScroll from "./scroll/UserGuildScroll";
 
 const Sidebar = ({ auth }: { auth: any }) => {
-    const [userServers, setUserServers] = useState(false);
+    const [userServers] = useState(false);
     const scrollParent = useRef(null);
 
     return (
         <div className="sidebar-container">
-            {auth && (userServers ? (
-                <Button variant="outlined" sx={{ backgroundColor: "#3c1f41" }} onClick={() => setUserServers(false)}>Your
-                    Servers</Button>) : (
-                <Button variant="outlined" color="success" onClick={() => setUserServers(true)}>Bot&apos;s
-                    Servers</Button>))}
             <div className={auth ? "sidebar-scroll-container-auth" : "sidebar-scroll-container"} ref={scrollParent}>
                 {userServers && auth ? (
                     <UserGuildScroll parent={scrollParent} />
@@ -28,5 +21,13 @@ const Sidebar = ({ auth }: { auth: any }) => {
         </div>
     );
 };
+
+/**
+ * {auth && (userServers ? (
+ *                 <Button variant="outlined" sx={{ backgroundColor: "#3c1f41" }} onClick={() => setUserServers(false)}>Your
+ *                     Servers</Button>) : (
+ *                 <Button variant="outlined" color="success" onClick={() => setUserServers(true)}>Bot&apos;s
+ *                     Servers</Button>))}
+ */
 
 export default Sidebar;
